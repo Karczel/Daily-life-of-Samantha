2a7247d83a6a5e8fe99526a4fa007e9bd70f5ba5
Karczel <92095164+Karczel@users.noreply.github.com>
2022-11-26 17:38:42 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Daily-life-of-Samantha
+ This is a console-based role-playing game, where you'll be roleplaying as Samantha!
---
+++ main.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.pu()
+turtle.forward(100)
+turtle.backward(100)
+turtle.pendown()
+turtle.forward(100)
+turtle.forward(100)
