83a31fe813c849804b050d8702e93c60043807e6
Karczel <92095164+Karczel@users.noreply.github.com>
2022-11-27 09:12:41 +0700

    Fix directory name and Project name
    Originally going to name this Daily life of Samantha Graphics but changed my mind later, I only changed the name in folder, run into some problems describe below
    
    There was some kind of error with the run configurator but I kept changing the Project interpreter to match the new project name (I tried changing the directory name in pycharm first with refractor but it didn't work)
    Using Ctrl + Alt + S and go to project interpreter and open a test run python to compare and match path
    
    Bug fixed

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-class Samantha:
-    def __init__(self):
-        self. =
+import sys
+import time
 
-class Karczel:
-    def __init__(self):
-
-class Omisha:
-    def __init__(self):
-
-class Zahur:
-    def __init__(self):
-
-class Table:
-    def __init__(self):
-
-class Person:
-    def __init__(self):
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1./10)
+slowprint("Hello.")
+# class Samantha:
+#     def __init__(self):
+#         self. =
+#
+# class Karczel:
+#     def __init__(self):
+#
+# class Omisha:
+#     def __init__(self):
+#
+# class Zahur:
+#     def __init__(self):
+#
+# class Table:
+#     def __init__(self):
+#
+# class Person:
+#     def __init__(self):
+#
+# # --- Start ---
